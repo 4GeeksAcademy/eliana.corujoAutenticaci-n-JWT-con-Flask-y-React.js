f36b99a0bf5b75a175aceb9e1a8425035cecc5a6
Eliana Corujo <elianamariacorujo@gmail.com>
2026-01-07 02:35:59 +0000

    ejercicio terminado

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token # Importamos JWT
 
 api = Blueprint('api', __name__)
 CORS(api)
@@ -16,7 +17,6 @@ def handle_hello():
 @api.route('/signup', methods=['POST'])
 def signup():
     body = request.get_json()
-
     if not body or 'email' not in body or 'password' not in body:
         return jsonify({"msg": "Faltan datos"}), 400
 
@@ -27,5 +27,24 @@ def signup():
     new_user = User(email=body['email'], password=body['password'], is_active=True)
     db.session.add(new_user)
     db.session.commit()
-
     return jsonify({"msg": "Usuario creado con éxito"}), 200
+
+# --- ESTA ES LA RUTA DE LOGIN ---
+@api.route('/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    if not body or 'email' not in body or 'password' not in body:
+        return jsonify({"msg": "Faltan datos"}), 400
+
+    user = User.query.filter_by(email=body['email']).first()
+
+    # Si no existe el usuario o la clave está mal
+    if user is None:
+        return jsonify({"msg": "Usuario no encontrado"}), 404
+    
+    if user.password != body['password']:
+        return jsonify({"msg": "Contraseña incorrecta"}), 401
+
+    # Crear token
+    access_token = create_access_token(identity=user.email)
+    return jsonify({"token": access_token, "user_id": user.id}), 200
